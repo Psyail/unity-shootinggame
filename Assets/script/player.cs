983a1bd9fcc6bd806bf942e1527909ffb6df172e
SSeongB <wsb0824@gmail.com>
2022-07-04 23:06:41 +0900

    Add a player script

--- Assets/script/player.cs
+++ Assets/script/player.cs
@@ -3,16 +3,33 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class player : MonoBehaviour
-{
-    // Start is called before the first frame update
+{   
+    public float Movespeed = 5.0f;
+    public GameObject PlayerObject;
+    public GameObject TargetObject;
     void Start()
     {
-        
+        Cursor.visible = false;
     }
 
-    // Update is called once per frame
     void Update()
     {
+        Movement();
+        RotateFollowMouse();
+    }
+    void Movement()
+    {
+        transform.Translate(Input.GetAxis("Vertical") * Vector2.up * Movespeed * Time.deltaTime);
+        transform.Translate(Input.GetAxis("Horizontal") * Vector2.right * Movespeed * Time.deltaTime);
+    }
+    void RotateFollowMouse()
+    {
+        Vector3 dir = Input.mousePosition - Camera.main.WorldToScreenPoint(PlayerObject.transform.position);
+        float angle = Mathf.Atan2(dir.y,dir.x) * Mathf .Rad2Deg +270;
+        PlayerObject.transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
         
+        Vector3 position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        position.z = 1;
+        TargetObject.transform.position = position;
     }
 }
